b2e823fbfb9b82beee79d679200901a9f657fae9
DNAScanner <75182303+DNAScanner@users.noreply.github.com>
2023-03-21 22:03:22 +0100

    Removed debug line

--- index.js
+++ index.js
@@ -28,25 +28,9 @@ function update() {
 	let timeSinceLastScroll = Date.now() - lastTimeScrolled;
 
 	if (timeSinceLastScroll > 500 && !autoScrollHappened && autoScrollOn) {
-		// Create a red line, thats in the center of the screen
-		for (let element of document.getElementsByClassName("temp")) {
-			element.remove();
-		}
-
-		let temp = document.createElement("div");
-		temp.style.position = "absolute";
-		temp.style.top = scrollHeight + "px";
-		temp.style.left = "0px";
-		temp.style.width = "100%";
-		temp.style.height = "2px";
-		temp.style.backgroundColor = "red";
-		temp.classList.add("temp");
-		document.body.appendChild(temp);
-
 		let topSection = null;
 		let bottomSection = null;
 
-		// Find the top and bottom sections visible in the viewport
 		for (let section of sections) {
 			let rect = section.getBoundingClientRect();
 
@@ -61,7 +45,6 @@ function update() {
 			}
 		}
 
-		// If top and bottom sections visible in the viewport are not the same, scroll to the top of the closest section
 		if (topSection && bottomSection && topSection !== bottomSection) {
 			let closestSection;
 			let closestDistance = Infinity;
@@ -100,7 +83,6 @@ function update() {
 update();
 
 for (let section of document.getElementById("content").children) {
-	// Create an <a> element
 	let link = document.createElement("a");
 	link.href = `#${section.id}`;
 	link.innerText = section.id.charAt(0).toUpperCase() + section.id.slice(1);
@@ -174,34 +156,28 @@ const socials = [
 ];
 
 for (let social of socials) {
-	// First, create a container for the card
 	let container = document.createElement("a");
 	container.classList.add("socials-card-container");
 	container.href = social.link;
 	container.target = "_blank";
 	container.rel = "noopener noreferrer";
 
-	// Then, create an element for the icon
 	let icon = document.createElement("img");
 	icon.classList.add("socials-card-icon");
 	icon.src = `./platforms/${social.icon}.svg`;
 	icon.alt = social.title + " icon";
 
-	// Then, create an element for the title
 	let title = document.createElement("h1");
 	title.classList.add("socials-card-title");
 	title.innerText = social.title;
 
-	// Then, create an element for the link
 	let link = document.createElement("h1");
 	link.classList.add("socials-card-link");
 	link.innerText = social.link;
 
-	// Append the components
 	container.appendChild(icon);
 	container.appendChild(title);
 	container.appendChild(link);
 
-	// And append the card to the socials container
 	document.getElementById("cards").appendChild(container);
 }
