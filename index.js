ae299eaaf71579443c16d0a844d057e0245e51ea
DNAScanner <75182303+DNAScanner@users.noreply.github.com>
2023-03-21 22:01:29 +0100

    Fixed auto-scrolling issue

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
-let lastTimeScrolled = Date.now();
+let sections = document.getElementById("content").children;
 let autoScrollHappened = true;
+let lastTimeScrolled = Date.now();
+let lastScrollHeight = 0;
+let autoScrollOn = true;
 
 document.addEventListener("scroll", () => {
 	lastTimeScrolled = Date.now();
 	autoScrollHappened = false;
-})
+});
 
 function update() {
-	let timeSinceLastScroll = Date.now() - lastTimeScrolled;
-
-	if (timeSinceLastScroll > 500 && !autoScrollHappened) {
-		let sections = document.getElementById("content").children;
-		let scrollHeight = window.scrollY + window.innerHeight * 0.5;
-
-		for (let section of sections) {
-			let sectionTop = section.offsetTop;
-			let sectionBottom = section.offsetTop + section.offsetHeight;
-
-			if (scrollHeight >= sectionTop && scrollHeight <= sectionBottom) {
-				let sectionId = section.id;
-
-				window.scrollTo({
-					top: sectionTop
-				})
-
-				break;
-			}
-		}
-
-		autoScrollHappened = true;
-	}
+	let scrollHeight = window.scrollY + window.innerHeight * 0.5;
 
 	if (location.hash) {
 		let section = document.getElementById(location.hash.slice(1));
@@ -39,8 +20,77 @@ function update() {
 			location.hash = "";
 
 			window.scrollTo({
-				top: sectionTop
-			})
+				top: sectionTop,
+			});
+		}
+	}
+
+	let timeSinceLastScroll = Date.now() - lastTimeScrolled;
+
+	if (timeSinceLastScroll > 500 && !autoScrollHappened && autoScrollOn) {
+		// Create a red line, thats in the center of the screen
+		for (let element of document.getElementsByClassName("temp")) {
+			element.remove();
+		}
+
+		let temp = document.createElement("div");
+		temp.style.position = "absolute";
+		temp.style.top = scrollHeight + "px";
+		temp.style.left = "0px";
+		temp.style.width = "100%";
+		temp.style.height = "2px";
+		temp.style.backgroundColor = "red";
+		temp.classList.add("temp");
+		document.body.appendChild(temp);
+
+		let topSection = null;
+		let bottomSection = null;
+
+		// Find the top and bottom sections visible in the viewport
+		for (let section of sections) {
+			let rect = section.getBoundingClientRect();
+
+			if (rect.top < window.innerHeight && rect.bottom > 0) {
+				if (!topSection || rect.top > topSection.getBoundingClientRect().top) {
+					topSection = section;
+				}
+
+				if (!bottomSection || rect.bottom < bottomSection.getBoundingClientRect().bottom) {
+					bottomSection = section;
+				}
+			}
+		}
+
+		// If top and bottom sections visible in the viewport are not the same, scroll to the top of the closest section
+		if (topSection && bottomSection && topSection !== bottomSection) {
+			let closestSection;
+			let closestDistance = Infinity;
+
+			for (let section of [topSection, bottomSection]) {
+				let sectionTop = section.offsetTop;
+				let sectionBottom = sectionTop + section.offsetHeight - 1;
+				let distance = Math.min(Math.abs(scrollHeight - sectionTop), Math.abs(scrollHeight - sectionBottom));
+
+				if (distance < closestDistance) {
+					closestDistance = distance;
+					closestSection = section;
+				}
+			}
+
+			if (closestSection) {
+				if (Math.abs(closestSection.offsetTop - scrollHeight) > Math.abs(closestSection.offsetTop + closestSection.offsetHeight - 1 - scrollHeight)) {
+					window.scrollTo({
+						top: closestSection.offsetTop + closestSection.offsetHeight - window.innerHeight,
+					});
+				} else {
+					window.scrollTo({
+						top: closestSection.offsetTop,
+					});
+				}
+			}
+
+			autoScrollHappened = true;
+			lastScrollHeight = scrollHeight;
 		}
 	}
 
@@ -124,34 +174,34 @@ const socials = [
 ];
 
 for (let social of socials) {
-      // First, create a container for the card
-      let container = document.createElement("a");
-      container.classList.add("socials-card-container");
-      container.href = social.link;
-      container.target = "_blank";
-      container.rel = "noopener noreferrer";
+	// First, create a container for the card
+	let container = document.createElement("a");
+	container.classList.add("socials-card-container");
+	container.href = social.link;
+	container.target = "_blank";
+	container.rel = "noopener noreferrer";
 
-      // Then, create an element for the icon
-      let icon = document.createElement("img");
-      icon.classList.add("socials-card-icon")
-      icon.src = `./platforms/${social.icon}.svg`;
-      icon.alt = social.title + " icon";
+	// Then, create an element for the icon
+	let icon = document.createElement("img");
+	icon.classList.add("socials-card-icon");
+	icon.src = `./platforms/${social.icon}.svg`;
+	icon.alt = social.title + " icon";
 
-      // Then, create an element for the title
-      let title = document.createElement("h1");
-      title.classList.add("socials-card-title");
-      title.innerText = social.title;
+	// Then, create an element for the title
+	let title = document.createElement("h1");
+	title.classList.add("socials-card-title");
+	title.innerText = social.title;
 
-      // Then, create an element for the link
-      let link = document.createElement("h1");
-      link.classList.add("socials-card-link");
-      link.innerText = social.link;
+	// Then, create an element for the link
+	let link = document.createElement("h1");
+	link.classList.add("socials-card-link");
+	link.innerText = social.link;
 
-      // Append the components
-      container.appendChild(icon);
-      container.appendChild(title);
-      container.appendChild(link);
+	// Append the components
+	container.appendChild(icon);
+	container.appendChild(title);
+	container.appendChild(link);
 
-      // And append the card to the socials container
-      document.getElementById("cards").appendChild(container);
+	// And append the card to the socials container
+	document.getElementById("cards").appendChild(container);
 }
