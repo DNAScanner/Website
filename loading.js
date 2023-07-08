db386a25e1a3b79872be2c00f45ec8d49dbe1ab1
DNAScanner <75182303+DNAScanner@users.noreply.github.com>
2023-07-08 20:57:55 +0200

    Added style (i forgor)

--- loading.js
+++ loading.js
@@ -1,3 +1,15 @@
+const style = document.createElement("style");
+style.innerHTML = `
+loading > * {animation: loading 1.5s infinite}
+loading > span:nth-child(2) {animation-delay: 0.2s}
+loading > span:nth-child(3) {animation-delay: 0.4s}
+@keyframes loading {
+	0% {opacity: 1}
+	50% {opacity: 0}
+	100% {opacity: 1}
+}
+`;
+document.head.appendChild(style);
 const loading = Array.from(document.getElementsByTagName("loading"));
 
 for (const element of loading) {
