85a674c59cd214da276bde4d34dfe235f825dbf0
DNAScanner <75182303+DNAScanner@users.noreply.github.com>
2023-08-26 03:27:39 +0200

    Added free to use render function for deno

---
+++ functions/deno/render.ts
@@ -0,0 +1,24 @@
+import * as path from "https://deno.land/std@0.197.0/path/mod.ts";
+import chalk from "npm:chalk"
+
+const render = (file: string, variables: Record<string, string>, ignoreNonReplacedVariables?: boolean) => {
+	if (ignoreNonReplacedVariables == null) ignoreNonReplacedVariables = false;
+
+	let text;
+	try {
+		text = Deno.readTextFileSync(path.join(Deno.cwd(), file));
+	} catch {
+		throw new Error(`Requested file doesnt exist (${file}}`);
+	}
+
+	for (const key in variables) {
+		const pattern = new RegExp(`{{( +)?${key}( +)?}}`, `gi`);
+		text = text.replaceAll(pattern, variables[key]);
+	}
+
+	if (!ignoreNonReplacedVariables && text.match(/{{.+}}/gi)) throw new Error(`Non replaced variable found in ${chalk.blue(`file:///${path.join(Deno.cwd(), file)}`)}\n> ${text.match(/{{.+}}/gi)?.join("\n> ")}`);
+
+	return text;
+};
+
+export default render;
